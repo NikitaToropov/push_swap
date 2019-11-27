b318e9a786ff69494cb068754fe3f34583c51ac7
Clement Missy <cmissy@vo-l7.21-school.ru>
2019-11-27 18:12:34 +0300

    reboot

--- push_swap.c
+++ push_swap.c
@@ -6,10 +6,15 @@ int		main(int c, char **v)
 	t_stack		*b;
 	int			*arr;
 	int			len;
+	int			i;
 
-
-	if (c == 2)
+	if (c >= 2)
 	{
+		i = 0
+		while (i < c)
+		{
+			i++;
+		}
 		if (!(arr = ft_int_arr(v[1], &len)))
 		{
 			write(1, "Error\n", 6);
@@ -26,5 +31,23 @@ int		main(int c, char **v)
 		// ft_sorting(a, b);
 		ft_free_stacks(&a, &b);
 	}
+	// if (c == 2)
+	// {
+	// 	if (!(arr = ft_int_arr(v[1], &len)))
+	// 	{
+	// 		write(1, "Error\n", 6);
+	// 		write(1, "there\n", 6);
+	// 		return (0);
+	// 	}
+	// 	if (ft_a_is_sorted(arr, len))
+	// 	{
+	// 		free(arr);
+	// 		return (0);
+	// 	}
+	// 	ft_make_two_stacks(&a, &b, arr, len);
+	// 	ft_new_sorting(a, b);
+	// 	// ft_sorting(a, b);
+	// 	ft_free_stacks(&a, &b);
+	// }
 	return (0);
 }
