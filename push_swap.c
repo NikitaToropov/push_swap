8a54527640582fefd38a76ca52c63eebe5bedfcd
Clement Missy <cmissy@vo-l7.21-school.ru>
2019-11-27 19:59:55 +0300

    add variator

--- push_swap.c
+++ push_swap.c
@@ -5,22 +5,26 @@ int		main(int c, char **v)
 	t_stack		*a;
 	t_stack		*b;
 	int			*arr;
+	char		*str;
 	int			len;
 	int			i;
 
 	if (c >= 2)
 	{
-		i = 0
+		str = ft_strdup(v[1]);
+		i = 2;
 		while (i < c)
 		{
+			str = ft_strjoin_free(str, ft_strjoin(" ", v[i]));
 			i++;
 		}
-		if (!(arr = ft_int_arr(v[1], &len)))
+		if (!(arr = ft_int_arr(str, &len)))
 		{
+			free(str);
 			write(1, "Error\n", 6);
-			write(1, "there\n", 6);
 			return (0);
 		}
+			free(str);
 		if (ft_a_is_sorted(arr, len))
 		{
 			free(arr);
@@ -28,7 +32,8 @@ int		main(int c, char **v)
 		}
 		ft_make_two_stacks(&a, &b, arr, len);
 		ft_new_sorting(a, b);
-		// ft_sorting(a, b);
+		// print_stacks(a, b);
+		ft_sorting(a, b);
 		ft_free_stacks(&a, &b);
 	}
 	// if (c == 2)
